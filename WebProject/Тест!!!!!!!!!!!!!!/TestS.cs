72a3b44031ea7325ae9bca773ae76b129ca08978
AizekMaster <reepushkamaster1@mail.ru>
2020-08-10 19:14:09 +0300

    Добавлен класс и папка (Test/Тест)

---
+++ WebProject/Тест!!!!!!!!!!!!!!/TestS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProject.Тест______________
+{
+    public class TestS
+    {
+        public void method()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
